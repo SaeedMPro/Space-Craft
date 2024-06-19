180f24f41e265313da9d0ed3c56821a7fb787ccb
acontius <ahuram188@gmail.com>
2024-06-19 21:28:31 +0330

    error handeled

--- python/main.py
+++ python/main.py
@@ -9,30 +9,38 @@ class Location:
     y: int
 
 def read_file(file_path: str) -> tuple:
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        
-        size   = list(map(int, lines[0].strip().split()))
-        info   = list(map(int, lines[1].strip().split()))
-        location = Location(info[0], info[1])
-        energy = info[2]
-        
-        space_map = []
-        for line in lines[2:]:
-            row = list(map(int, line.strip().split()))
-            space_map.append(row)
-        
-        return size, location, energy, space_map
+    try :
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            
+            size   = list(map(int, lines[0].strip().split()))
+            info   = list(map(int, lines[1].strip().split()))
+            location = Location(info[0], info[1])
+            energy = info[2]
+            
+            space_map = []
+            for line in lines[2:]:
+                row = list(map(int, line.strip().split()))
+                space_map.append(row)
+            
+            return size, location, energy, space_map
+    except FileNotFoundError : 
+        raise FileNotFoundError("Couldn't find that shit")
+
+
 
 def main() -> None:
-    size, location, energy, space_map = read_file(testcase)
-    
-    print(f"Size: {size}")
-    print(f"Location: {location}")
-    print(f"Initial Energy: {energy}")
-    print("Space map:")
-    for row in space_map:
-        print(*row)
+    try :
+        size, location, energy, space_map = read_file(testcase)
+        
+        print(f"Size: {size}")
+        print(f"Location: {location}")
+        print(f"Initial Energy: {energy}")
+        print("Space map:")
+        for row in space_map:
+            print(*row)
 
+    except FileNotFoundError as fnfe :
+        print(fnfe)
 if __name__ == "__main__":
     main()
