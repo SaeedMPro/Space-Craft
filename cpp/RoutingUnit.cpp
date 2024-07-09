ed00a023f333b08cb041973362e0d9cc77fb6fe4
acontius <ahuram188@gmail.com>
2024-07-09 17:35:17 +0330

    Read from console added!

--- cpp/RoutingUnit.cpp
+++ cpp/RoutingUnit.cpp
@@ -10,57 +10,49 @@ using namespace std;
 void RoutingUnit::startRU() {
     cout << "Routing Unit started" << endl;
     
-    vector<int> size;
+    vector<int> size(2);
     Cardinal location;
     int energy;
     vector<vector<char>> spaceMap;
 
+    bool ReadFile = false;
+
     //Reading input from "testcase.txt":
-    try {
-        auto result = readInputFile("testcase.txt");
-        size = get<0>(result);
-        location = get<1>(result);
-        energy = get<2>(result);
-        spaceMap = get<3>(result);
-        // cout << "Size: ";
-        // for (const auto& s : size) {
-        //     cout << s << " ";
-        // }
-        // cout << endl;
-        // cout << "Location: (" << location.x << ", " << location.y << ")" << endl;
-        // cout << "Initial Energy: " << energy << endl;
-        // cout << "Space map:" << endl;
-        // for (const auto& row : spaceMap) {
-        //     for (const auto& val : row) {
-        //         cout << val << " ";
-        //     }
-        //     cout << endl;
-        // }
-
-        
-    } catch (const runtime_error& e) {
+    if (ReadFile){
+        try {
+            auto result = readInputFile("testcase.txt");
+            size     = get<0>(result);
+            location = get<1>(result);
+            energy   = get<2>(result);
+            spaceMap = get<3>(result);
+        } 
+            catch (const runtime_error& e) {
+            cerr << "File not exists !" << endl;
+            cerr << e.what() << endl;
+            return;
+        }
+    } else {
+        try {
+            cin >> size[0] >> size[1];
+            cin >> location.x >> location.y;
+            cin >> energy;
+            spaceMap.resize(size[0]);
+            for (int i = 0; i < size[0]; i++){
+                string line;
+                cin >> ws;
+                getline(cin,line);
+                for(char ch : line){
+                    if(ch != ' '){
+                        spaceMap[i].push_back(ch);
+                }
+            }
+        } 
+    }catch(const runtime_error& e){
+        cerr << "Wrong inputes !" << endl;
         cerr << e.what() << endl;
+        return;
+        }
     }
-
-    //input from console:
-    // cin >> size[0] >> size[1];
-    // cin >> location.x >> location.y;
-    // cin >> energy;
-    // for (int i = 0; i < size[0]; i++) 
-    // {   
-    //     string line;
-    //     vector<char> row;
-    //     cin >> line;
-
-    //     for (char ch : line) {
-    //         if (ch != ' ') { 
-    //             row.push_back(ch);
-    //         }
-    //     }
-    //     spaceMap.push_back(row);
-    // }
-
-
     // Initialize SpaceCraft and Map according to input.
     craft = new SpaceCraft(energy, location);
     currentMap = new Map(spaceMap, size, craft);
